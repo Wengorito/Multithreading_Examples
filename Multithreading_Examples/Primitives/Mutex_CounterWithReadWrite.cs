6d7cca46c047e517eb2312a128e68fca43a6e4fb
Grzegorz <hvitr.ulfr@gmail.com>
2025-11-03 09:13:22 +0100

    Add Mutex example.

---
+++ Multithreading_Examples/Primitives/Mutex_CounterWithReadWrite.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+namespace Multithreading_Examples.Primitives
+{
+    internal class Mutex_CounterWithReadWrite
+    {
+        string _filePath = "counter.txt";
+
+        // run multiple example instances simultaneously (.exe in \bin folder) to see the effect
+        internal Mutex_CounterWithReadWrite()
+        {
+            this.Increment(_filePath);
+
+            Process.Start("notepad.exe", _filePath);
+            Console.WriteLine("Process finished.");
+            Console.ReadLine();
+        }
+
+        void Increment(string filePath)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                // Mutex allows exclusion across different processes
+                using (var mutex = new Mutex(false, $"GlobalFileMutex:{filePath}"))
+                {
+                    mutex.WaitOne();
+                    try
+                    {
+                        int counter = ReadCounter(filePath);
+                        counter++;
+                        WriteCounter(filePath, counter);
+                    }
+                    finally
+                    {
+                        mutex.ReleaseMutex();
+                    }
+                }
+            }
+        }
+
+        int ReadCounter(string filePath)
+        {
+            using (var stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite))
+            using (var reader = new StreamReader(stream))
+            {
+                string content = reader.ReadToEnd();
+                return string.IsNullOrEmpty(content) ? 0 : int.Parse(content);
+            }
+        }
+
+        void WriteCounter(string filePath, int counter)
+        {
+            using (var stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write(counter);
+            }
+        }
+    }
+}
--- Multithreading_Examples/Program.cs
+++ Multithreading_Examples/Program.cs
@@ -1,35 +1,47 @@
-﻿Console.WriteLine("Enter a command to run a multithreading primitive example:\n\n" +
-    "1. simple 2-threaded counter (monitor)\n2. multi-threaded server simulation (lock)\n3. tickets booking system (monitor with timeout)\n4. mutex\n5. semaphore\n6. autoresetevent\n7. manualresetevent\nor 'exit' to quit:");
+﻿
+Console.WriteLine("Enter a command to run a multithreading primitive example:\n\n" +
+"1. simple 2-threaded counter (monitor)\n2. multi-threaded server simulation (lock)\n3. tickets booking system (monitor with timeout)\n" +
+"4. mutex\n5. semaphore\n6. autoresetevent\n7. manualresetevent\nor 'exit' to quit:\n\n");
 
-string? input = Console.ReadLine();
-if (input?.ToLower() == "exit")
+while (true)
 {
-    return;
+    Console.BackgroundColor = ConsoleColor.Cyan;
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine("Type your command and press enter: ");
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.White;
+
+    string? input = Console.ReadLine();
+    if (input?.ToLower() == "exit")
+    {
+        break;
+    }
+    switch (input)
+    {
+        case "1":
+            new Multithreading_Examples.Primitives.Monitor_Counter();
+            break;
+        case "2":
+            new Multithreading_Examples.Primitives.Lock_WebServer();
+            break;
+        case "3":
+            new Multithreading_Examples.Primitives.MonitorTimeout_TicketsBooking();
+            break;
+        case "4":
+            new Multithreading_Examples.Primitives.Mutex_CounterWithReadWrite();
+            break;
+        //case "semaphore":
+        //    new Multithreading_Examples.Primitives.SemaphoreExample();
+        //    break;
+        //case "autoresetevent":
+        //    new Multithreading_Examples.Primitives.AutoResetEventExample();
+        //    break;
+        //case "manualresetevent":
+        //    new Multithreading_Examples.Primitives.ManualResetEventExample();
+        //    break;
+        default:
+            Console.WriteLine("Unknown command. Available commands: lock, monitor, mutex, semaphore, autoresetevent, manualresetevent");
+            break;
+    }
 }
-switch (input)
-{
-    case "1":
-        new Multithreading_Examples.Primitives.Monitor_Counter();
-        break;
-    case "2":
-        new Multithreading_Examples.Primitives.Lock_WebServer();
-        break;
-    case "3":
-        new Multithreading_Examples.Primitives.MonitorTimeout_TicketsBooking();
-        break;
-    //case "mutex":
-    //    new Multithreading_Examples.Primitives.MutexExample();
-    //    break;
-    //case "semaphore":
-    //    new Multithreading_Examples.Primitives.SemaphoreExample();
-    //    break;
-    //case "autoresetevent":
-    //    new Multithreading_Examples.Primitives.AutoResetEventExample();
-    //    break;
-    //case "manualresetevent":
-    //    new Multithreading_Examples.Primitives.ManualResetEventExample();
-    //    break;
-    default:
-        Console.WriteLine("Unknown command. Available commands: lock, monitor, mutex, semaphore, autoresetevent, manualresetevent");
-        break;
-}
+
